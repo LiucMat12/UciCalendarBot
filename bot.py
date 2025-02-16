b1f063f1b8f4e352e09378e7627b763aaa86eb87
LiucMat12 <lucamattiols@gmail.com>
2025-02-16 13:15:47 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,17 +3,22 @@ import datetime
 import time
 import schedule
 from telegram import Bot
+from datetime import datetime, timedelta
+import os
 
-TOKEN = "7981437591:AAGQqWOzccytWLRC6HDKPQtRgzJAaxaWAbI"
-CHAT_ID = "916301720"
-FILE_CSV = "eventi.csv"
+# Ottieni il token del bot e chat ID dalle variabili d'ambiente
+TOKEN_BOT = os.getenv('TOKEN_BOT')
+CHAT_ID = os.getenv('CHAT_ID')
 
-bot = Bot(token=TOKEN)
+bot = Bot(token=TOKEN_BOT)
 
 def invia_promemoria():
-    oggi = datetime.date.today()
-    domani = oggi + datetime.timedelta(days=1)
-    eventi = pd.read_csv(FILE_CSV)
+    # Percorso al file CSV
+    file_csv = '/app/repository/eventi.csv'  # Cambia con il percorso effettivo
+    eventi = pd.read_csv(file_csv)
+
+oggi = datetime.now()
+    domani = oggi + timedelta(days=1)
 
     for _, row in eventi.iterrows():
         data_evento = datetime.datetime.strptime(row["data"], "%Y-%m-%d").date()
@@ -21,12 +26,14 @@ def invia_promemoria():
             messaggio = f"🔔 *Promemoria Evento*\n\n📅 Data: {row['data']}\n🏷 *{row['titolo']}*\n📝 {row['descrizione']}"
             bot.send_message(chat_id=CHAT_ID, text=messaggio, parse_mode="Markdown")
 
-schedule.every().day.at("09:00").do(invia_promemoria)
+# Pianifica l'esecuzione del promemoria una volta al giorno
+schedule.every().day.at("09:00").do(invia_promemoria)  # Esegui ogni giorno alle 9:00 AM
 
-schedule.every().day.at("00:01").do(invia_promemoria)
+schedule.every().day.at("12:00").do(invia_promemoria)  # Esegui ogni giorno alle 12:00 AM
 
 if __name__ == "__main__":
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
+    # Loop infinito per eseguire il controllo
+while True:
+    schedule.run_pending()
+    time.sleep(60)  # Attendi un minuto prima di controllare di nuovo
 
